ce8dc2bfd1bb97b2bf7ae74b68d7257e36c9873d
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-06-24 17:20:16 +0600

    LUCKYSEVEN problem solved

---
+++ src/LUCKYSEVEN.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class LUCKYSEVEN {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        String s = scanner.nextLine();
+
+        System.out.println(s.charAt(8));
+
+        scanner.close();
+    }
+}
