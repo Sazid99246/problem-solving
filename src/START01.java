4ac7d85f04fefa8050ff844fdae7bf3c1725ad34
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-06-24 17:13:22 +0600

    START01 problem solved

---
+++ src/START01.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class START01 {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.println(n);
+        s.close();
+    }
+}
