d59a3e27d4c3474824561b0b7b4321727cf5d728
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-01 23:43:25 +0600

    3 problems solved

---
+++ START01.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    // Do not print anything before taking input
+    cin >> a;
+    
+    // Output the value of a in the line below
+    cout << a;
+}
