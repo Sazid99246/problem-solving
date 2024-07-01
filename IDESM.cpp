f3a82aa47fa5ec5bb064d8169d85a4fc53a97c85
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 05:59:35 +0600

    IDESM problem solved

---
+++ IDESM.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 15)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
