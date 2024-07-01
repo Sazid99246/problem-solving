4cbd8c905c06d3008c81e013b6c8ec382866c11b
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 05:35:56 +0600

    CHRISTGREET problem solved

---
+++ CWC23QUALIF.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x >= 12)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
