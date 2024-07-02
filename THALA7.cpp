970d3a79ba4e99cf7cc93973cc8761d085701a94
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 06:11:21 +0600

    THALA7 problem solved

---
+++ THALA7.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 7)
+    {
+        cout << "THALA" << endl;
+    }
+    else
+    {
+        cout << "SADGE" << endl;
+    }
+}
