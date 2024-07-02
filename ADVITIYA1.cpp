eba9ff05aaa4ff87a6467f98f3b2fc68c6341539
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 06:37:37 +0600

    ADVITIYA problem solved

---
+++ ADVITIYA1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x == 16 || x == 17 || x == 18)
+    {
+        cout << "ADVITIYA";
+    }
+    else
+    {
+        cout << "WAITING FOR ADVITIYA";
+    }
+    cout << endl;
+}
