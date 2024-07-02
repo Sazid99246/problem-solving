43862b0ac0ea2394a70e3ff56ca61e5c14bc0282
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 07:28:32 +0600

    ACPLZ problem solved

---
+++ ACPLZ.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    if (t > 30)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    cout << endl;
+}
