4ac5c13341a3ba8d4a1662288f961a8e39f8da52
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 07:32:29 +0600

    ELHP problem solved

---
+++ ELHP.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x >= y * 2)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    cout << endl;
+}
