29f3c0f2129163aa056702fd9cbcb7091b299ca3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 06:30:14 +0600

    MANGOLASSI problem solved

---
+++ MANGOLASSI.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x > 35)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    cout << endl;
+}
