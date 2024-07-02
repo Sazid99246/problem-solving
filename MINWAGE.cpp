4fd937b26fd4d011bd7fd0b263c13e6f416f8c65
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-02 07:34:58 +0600

    MINEAGR problem solved

---
+++ MINWAGE.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x > 11)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+    cout << endl;
+}
