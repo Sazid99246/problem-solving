f910d3018ec7fc498559448a9a5320d2ef9dc160
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-03 23:06:51 +0600

    HEALSE problem solved

---
+++ HEALSE.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 8)
+    {
+        cout << "LESS" << endl;
+    }
+    else if (n > 8)
+    {
+        cout << "MORE" << endl;
+    }
+    else
+    {
+        cout << "PERFECT" << endl;
+    }
+    
+}
