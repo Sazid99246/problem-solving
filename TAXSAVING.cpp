9b11a83dd393f0117e1e2eee8b22bd0a1b6ffb33
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-03 23:13:19 +0600

    TAXSAVING problem solved

---
+++ TAXSAVING.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, y, x;
+    cin >> t;
+    while (t--)
+    {
+        cin >> y >> x;
+        cout << y - x << endl;
+    }
+    
+}
