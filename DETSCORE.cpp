27f15c0bd4fd60360c14a6f9bb12ed19fc1136d2
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-07 18:37:59 +0600

    DETSCORE problem solved

---
+++ DETSCORE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, n;
+        cin >> x >> n;
+        cout << (x / 10) * n << endl;
+    }
+}
