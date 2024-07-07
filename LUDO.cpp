652e9273bcf4967233553008dd74d0a1706483b4
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-07 13:18:53 +0600

    LUDO problem solved

---
+++ LUDO.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        if (x == 6)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+        
+    }
+    
+}
