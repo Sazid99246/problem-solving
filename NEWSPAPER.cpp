b473d8338548bb8c9c994c5767fcc64445d76ef3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-08 05:36:23 +0600

    NEWSPAPER problem solved

---
+++ NEWSPAPER.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string result = n >= 8 ? "YES" : "NO";
+    cout << result << endl;
+
+    return 0;
+}
