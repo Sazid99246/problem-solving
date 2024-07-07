d7f30f428957eb3cc3a46b4a0928b0b3078fd9ec
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-07 13:08:37 +0600

    BRIYANI problem solved

---
+++ BIRYANI.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x * y << endl;
+    }
+    
+}
