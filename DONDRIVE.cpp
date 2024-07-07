a9d16960d5f5c0231beff81ee1724d643137f0d9
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-07 22:39:38 +0600

    DONDRIVE problem solved

---
+++ DONDRIVE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, x;
+        cin >> n >> x;
+        cout << n - x << endl;
+    }
+    return 0;
+}
