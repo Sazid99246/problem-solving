6b9df150dac273b458def514c6d25c33e7832f55
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-08 18:59:47 +0600

    CCOV problem solved

---
+++ Codechef/CCOV.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    cin >> x;
+    string result = x > 40 ? "YES" : "NO";
+    cout << result << endl;
+    return 0;
+}
