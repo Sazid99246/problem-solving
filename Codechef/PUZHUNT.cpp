e52fd950524b855bdffa59434588ddc1dda05cc8
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-08 17:20:17 +0600

    PUZHUNT problem solved

---
+++ Codechef/PUZHUNT.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string result = n >=6 && n <= 8 ? "YES" : "NO";
+    cout << result << endl;
+    return 0;
+}
