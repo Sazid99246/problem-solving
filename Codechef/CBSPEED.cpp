11ede2690898d9a7deb1f98217b7f45321c235fc
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 05:49:53 +0600

    CBSPEED problem solved

---
+++ Codechef/CBSPEED.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    string result = y > x ? "YES" : "NO";
+    cout << result << endl;
+    return 0;
+}
