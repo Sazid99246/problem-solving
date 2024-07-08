cf183711ae0751b1dc0e1eebcd4a176c22de0107
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-08 17:24:55 +0600

    ALGOFINALS problem solved

---
+++ Codechef/ALGOFINALS.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    string result = x == 17 ? "YAY" : "NO";
+    cout << result << endl;
+    return 0;
+}
