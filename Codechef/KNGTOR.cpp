620be4084233cd0c61922dd5d2ff385b798dfda7
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-08 20:31:47 +0600

    KNGTOR problem solved

---
+++ Codechef/KNGTOR.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n, m;
+        cin >> n >> m;
+        cout << n * 5 + m * 7 << endl;
+    }
+}
