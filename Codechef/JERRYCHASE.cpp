6bd1b4a99ea5315e2d4569be9c30c3de16cb4e07
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 08:28:30 +0600

    JERRYCHASE problem solved

---
+++ Codechef/JERRYCHASE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        string result = y > x ? "YES" : "NO";
+        cout << result << endl;
+    }
+    return 0;
+}
