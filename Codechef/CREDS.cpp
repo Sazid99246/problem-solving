5fe8b02dd474cfd1acd2ce40f057f7a01aa61631
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 19:51:25 +0600

    CEWDS problem solved

---
+++ Codechef/CREDS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << x * 4 + y * 2 << endl;
+    }
+
+    return 0;
+}
