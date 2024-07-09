899fdbaab7533c8cea355aad209712932a486a9a
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 08:31:28 +0600

    AGEING problem solved

---
+++ Codechef/AGEING.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        cout << x - 10 << endl;
+    }
+    return 0;
+}
