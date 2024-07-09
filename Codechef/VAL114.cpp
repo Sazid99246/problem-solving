4add309271285953b26e3eb253801978cbc4a881
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 21:56:08 +0600

    VAL114 problem solved

---
+++ Codechef/VAL114.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string result = n == 121 ? "Likely" : "Unlikely";
+    cout << result << endl;
+    return 0;
+}
