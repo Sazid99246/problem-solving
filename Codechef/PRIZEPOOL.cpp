4ed594ec065aee08a18790ab4f9d227038a5dbdc
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 08:18:42 +0600

    PRIZEPOOL problem solved

---
+++ Codechef/PRIZEPOOL.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x * 10 + y * 90 << endl;
+    }
+    return 0;
+}
