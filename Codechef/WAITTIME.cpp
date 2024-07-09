08a871cdf9504cd37d817f5421fb51229cd54ea1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-09 22:00:37 +0600

    WAITTIME problem solved

---
+++ Codechef/WAITTIME.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int k, x;
+        cin >> k >> x;
+        cout << k * 7 - x << endl;
+    }
+
+    return 0;
+}
