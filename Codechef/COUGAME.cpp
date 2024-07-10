ad1e9de9ed47b49721922246b9064983eea3549c
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 10:25:21 +0600

    COUGAME problem solved

---
+++ Codechef/COUGAME.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int g, b;
+        cin >> g >> b;
+        cout << b - g << endl;
+    }
+
+    return 0;
+}
