59e91bf090f54f87c53862327fcd2c00e4dfc878
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 18:18:22 +0600

    POPULATION problem solved

---
+++ Codechef/POPULATION.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << x - y + z << endl;
+    }
+    
+    return 0;
+}
