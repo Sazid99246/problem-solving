c0c87fec336c57ad60fd8eaa4228cde2eb73c579
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 15:56:07 +0600

    LTIME problem solved

---
+++ Codechef/LTIME.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        string result = x >= 1 && x <= 4 ? "YES" : "NO";
+        cout << result << endl;
+    }
+
+    return 0;
+}
