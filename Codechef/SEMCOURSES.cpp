40405a0b989e04bc854c97b8fb41e83e002daf00
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 10:54:46 +0600

    SEMCOURSES problem solved

---
+++ Codechef/SEMCOURSES.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        int result = x * y * z;
+        cout << result << endl;
+    }
+
+    return 0;
+}
