2952c310d0035ada380c410c9bad4429dceb5b32
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 06:36:17 +0600

    LEGSP problem solved

---
+++ Codechef/LEGSP.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    string result = m - n > 0 ? "YES" : "NO";
+    cout << result << endl;
+    return 0;
+}
