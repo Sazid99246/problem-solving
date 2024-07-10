5aacd6ef4e214e8b6e086c511322a62f2b8edd24
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 18:23:52 +0600

    MARKSTW problem solved

---
+++ Codechef/MARKSTW.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    string result = x >= y * 2 ? "Yes" : "No";
+    cout << result << endl;
+    return 0;
+}
