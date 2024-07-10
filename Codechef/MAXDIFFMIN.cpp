70567e70fec0f7cb74dc46f3f9ad73ae5ab7d7fa
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 08:42:43 +0600

    MAXDIFFMIN problem solved

---
+++ Codechef/MAXDIFFMIN.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << max(a, (max(b, c))) - min(a, min(b, c)) << endl;
+    }
+
+    return 0;
+}
