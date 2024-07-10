b8d43989c7c8a5b099d9f9f500af462b4537fc8f
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 16:04:29 +0600

    NEWCC problem solved

---
+++ Codechef/NEWCC.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x, y;
+    cin >> x >> y;
+    if (y > x)
+    {
+        cout << "Old" << endl;
+    }
+    else if (y < x)
+    {
+        cout << "New" << endl;
+    }
+    else
+    {
+        cout << "Same" << endl;
+    }
+    
+
+    return 0;
+}
