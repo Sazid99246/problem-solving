6458d97de0c73c84a185e38d50a6baf61a651e81
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 10:46:55 +0600

    SLEEP problem solved

---
+++ Codechef/SLEEP.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        string result = x < 7 ? "YES" : "NO";
+        cout << result << endl;
+    }
+
+    return 0;
+}
