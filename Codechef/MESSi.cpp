ea757a57c5fc9ad58af6a43218220e8bd9550ef5
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-10 07:00:42 +0600

    MESSi problem solved

---
+++ Codechef/MESSi.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    string result = x > y ? "FREEKICK" : "PENALTY";
+    cout << result << endl;
+    return 0;
+}
