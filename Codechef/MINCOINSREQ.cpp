49bfb26b6ab41bff3e476cf82e6546fbea0558eb
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-11 12:53:29 +0600

    MINCOINSREQ problem solved

---
+++ Codechef/MINCOINSREQ.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        cout << n % 10 << endl;
+    }
+
+    return 0;
+}
