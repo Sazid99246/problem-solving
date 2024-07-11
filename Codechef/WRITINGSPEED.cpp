ecb6a02279df55f2da25d931a635c1a207cdd217
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-11 08:59:24 +0600

    WRITINGSPEED problem solved

---
+++ Codechef/WRITINGSPEED.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    string result = x * 5 <= 60 ? "YES" : "NO";
+    cout << result << endl;
+    return 0;
+}
