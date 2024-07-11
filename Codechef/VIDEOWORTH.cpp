7aa4ef528485a8649c52796be5120aef5d44c451
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-11 10:15:51 +0600

    VIDEOWORTH problem solved

---
+++ Codechef/VIDEOWORTH.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int s;
+        cin >> s;
+        cout << s * 24000 << endl;
+    }
+
+    return 0;
+}
