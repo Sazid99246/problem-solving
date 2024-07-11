c20f76a9b97d15118998a1944698a43b4fd8219a
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 05:59:31 +0600

    FBC problem oslved

---
+++ Codechef/FBC.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, k;
+        cin >> x >> k;
+        cout << x - k << endl;
+    }
+    
+    return 0;
+}
