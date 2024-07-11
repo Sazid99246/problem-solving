7bb5562cab6a7e3cded68e00d944fdb5af21a442
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-11 09:10:43 +0600

    WHOMAKESP1 problem solved

---
+++ Codechef/WHOMAKESP1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    string result = a <= b ? "Tyro" : "Dom";
+    cout << result << endl;
+    return 0;
+}
