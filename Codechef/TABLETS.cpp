ecd557b844df3c5c0e7ec222e3dd87fd70360651
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-11 10:03:01 +0600

    ONEFULPAIRS problem solved

---
+++ Codechef/TABLETS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        string result = y >= x * 3 ? "YES" : "NO";
+        cout << result << endl;
+    }
+
+    return 0;
+}
