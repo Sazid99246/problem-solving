8dba79f430af5006f0cc85719169882100d83ff7
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 09:34:54 +0600

    TVDISC problem solved

---
+++ Codechef/TVDISC.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        int firstTV = a - c;
+        int secondTV = b - d;
+        if (firstTV < secondTV)
+        {
+            cout << "First" << endl;
+        }
+        else if (secondTV < firstTV)
+        {
+            cout << "Second" << endl;
+        }
+        else
+        {
+            cout << "Any" << endl;
+        }
+        
+    }
+    
+    return 0;
+}
