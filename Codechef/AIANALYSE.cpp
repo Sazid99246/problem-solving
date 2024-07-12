854f77dcdf016265d2a0a60cd661b3f330ad0ec8
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 09:27:40 +0600

    AIANALYSE problem solved

---
+++ Codechef/AIANALYSE.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int c;
+    cin >> c;
+    string result = c <= 1000 ? "Yes" : "No";
+    cout << result << endl;
+    return 0;
+}
