2231ff18afd6c1e119c01aabf608b70068d3b300
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 11:52:08 +0600

    FLOW006 problem solved

---
+++ Codechef/FLOW006.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumOfDigits(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        cout << sumOfDigits(n) << endl;
+    }
+    
+    return 0;
+}
