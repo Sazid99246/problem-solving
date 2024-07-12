0f43f126aa1f18ddb593c6d9496f234651f70458
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 09:14:57 +0600

    MAXIMUMSUBS problem solved

---
+++ Codechef/MAXIMUMSUBS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        cout << x * 2 << endl;
+    }
+    
+    return 0;
+}
