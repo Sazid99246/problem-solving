167bfd302fdc03e99b2364ddbfd802d5e4e4da10
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-12 23:10:23 +0600

    BATTERYLOW problem solved

---
+++ Codechef/BATTERYLOW.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int x;
+        cin >> x;
+        string result = x <= 15 ? "Yes" : "No";
+        cout << result << endl;
+    }
+
+    return 0;
+}
