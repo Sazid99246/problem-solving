5c04856e8988f3a3f25d6cf4279a0ac3411e4b4b
Sheikh Md. Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2024-07-16 05:44:20 +0600

    MONOPOLYproblem solved

---
+++ Codechef/MONOPOLY.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+      int a, b, c;
+      cin >> a >> b >> c;
+      string result = (a > b + c) || (b > a + c) || (c > a + b) ? "Yes" : "No";
+      cout << result << endl;
+    }
+    return 0;
+    
+}
