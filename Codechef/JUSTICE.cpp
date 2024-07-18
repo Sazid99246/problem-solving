ded8bf1d37b266f90d854455d0caea5ef35e007f
Sheikh Md. Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2024-07-18 12:04:10 +0600

    JUSTICE problem solved

---
+++ Codechef/JUSTICE.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    string result = x >= y ? "YES" : "NO";
+    cout << result << endl;
+    
+    return 0;
+}
