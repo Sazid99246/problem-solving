af2d132f0620af8195b920eb8f65f817deae6a9b
Sheikh Md. Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2024-07-25 17:07:54 +0600

    NGRS problem solved

---
+++ Codechef/NGRS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+      int x, y;
+      cin >> x >> y;
+      
+      if (x < 50)
+      {
+        cout << "Z" << endl;
+      }
+      else
+      {
+        if (y < 50)
+        {
+          cout << "F" << endl;
+        }
+        else
+        {
+          cout << "A" << endl;
+        }
+      }
+    }
+    
+    return 0;
+}
