22c2f9c8565ff9aec336d1718b89d9f5663eaf8f
Sheikh Md. Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2024-07-26 10:30:57 +0600

    CREDCOINS problem solved

---
+++ Codechef/CREDCOINS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << (x * y) / 100 << endl;
+    }
+    return 0;
+}
