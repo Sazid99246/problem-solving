c2073412bf2121936d9e6510383928fbc6fa7dc1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-27 22:22:07 +0600

    PAINTCHRIS problem solved

---
+++ Codechef/PAINTCHRIS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        cout << z / (x * y * 2) << endl;
+    }
+    
+    return 0;
+}
