6edd5a2ead40d0051a45deb15e05b17ee1b25450
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-28 12:59:27 +0600

    CHOCDISTRIB problem solved

---
+++ Codechef/CHOCDISTRIB.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        double n;
+        cin >> n;
+        int max_children = ceil(n / 2);
+        cout << max_children << " " << n << endl;
+    }
+    
+    return 0;
+}
