82e39668e6b1a27964801a32f35e2c8c85808935
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-28 11:27:42 +0600

    FLOW007 problem solved

---
+++ Codechef/FLOW007.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string n;
+        cin >> n;
+
+        while (!n.empty() && n.back() == '0')
+        {
+            n.pop_back();
+        }
+        
+        reverse(n.begin(), n.end());
+        cout << n << endl;
+    }
+
+    return 0;
+}
