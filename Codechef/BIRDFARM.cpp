d19e83fc76269dcfc34b4c0d652b066fd3baf24e
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-28 11:39:28 +0600

    BIRDFARM problem solved

---
+++ Codechef/BIRDFARM.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        if (z % x != 0 && z % y != 0)
+        {
+            cout << "NONE" << endl;
+        }
+        else if (z % x == 0 && z % y == 0)
+        {
+            cout << "ANY" << endl;
+        }
+        else
+        {
+            if (z % x == 0)
+            {
+                cout << "CHICKEN" << endl;
+            }
+            else
+            {
+                cout << "DUCK" << endl;
+            }
+            
+        }
+        
+        
+    }
+    
+    return 0;
+}
