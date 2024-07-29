cfb784783fb0e6ce0bde0e6fadbe6e2fe4d19dc7
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 10:57:11 +0600

    BATH problem solved

---
+++ Codechef/BATH.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << x / (y * 2) << endl;
+    }
+    
+    return 0;
+}
