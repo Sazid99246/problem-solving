3913b51412e96367cfe83ada210e3d3c40997317
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 10:42:15 +0600

    POLTHIEF problem solved

--- Codechef/POLTHIEF.cpp
+++ Codechef/POLTHIEF.cpp
@@ -9,13 +9,13 @@ int main()
     {
         int x, y;
         cin >> x >> y;
-        
+
         if (x > y)
         {
             swap(x, y);
         }
         cout << y - x << endl;
     }
-    
+
     return 0;
 }
