59d0f7d4d6fa85c89bfcc0d56488a41c741c1642
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 10:49:35 +0600

    FLIPCARDS problem solved

---
+++ Codechef/FLIPCARDS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n,x;
+        cin >> n >> x;
+        int y = n - x;
+        cout << min(x, y) << endl;
+    }
+    
+    return 0;
+}
