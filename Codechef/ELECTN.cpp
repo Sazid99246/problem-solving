b1a0a3328ba2e6c49a62c548840d2c1b2f37ed33
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 08:24:56 +0600

    ELECTN problem solved

---
+++ Codechef/ELECTN.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, count = 0;
+
+        cin >> n >> x;
+
+        int *a = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] >= x)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+
+        delete[] a;
+    }
+
+    return 0;
+}
