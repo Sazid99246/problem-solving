a03fa22c6adfa317efa5d61cd9d9c5ff1d64691c
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 10:35:37 +0600

    POLTHIEF problem solved

---
+++ Codechef/POLTHIEF.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        
+        if (x > y)
+        {
+            swap(x, y);
+        }
+        cout << y - x << endl;
+    }
+    
+    return 0;
+}
