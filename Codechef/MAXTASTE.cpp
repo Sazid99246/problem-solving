e9963314bac6b286039ea3829d5d0d119739f997
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 10:19:34 +0600

    MAXTASTE problem solveed

---
+++ Codechef/MAXTASTE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << max(a + c, max(a + d, max(b + c, b + d))) << endl;
+    }
+
+    return 0;
+}
