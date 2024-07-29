c328180ad5ed68389f195e0b11acde2ef51a5522
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 08:59:04 +0600

    DISCUS problem solved

---
+++ Codechef/DISCUS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << max(a, max(b, c)) << endl;
+    }
+
+    return 0;
+}
