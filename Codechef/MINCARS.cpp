535cbd38d1f83dd6d436340b486a50cbc315fc29
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-29 08:30:16 +0600

    MINCARS problem solved

---
+++ Codechef/MINCARS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        double n;
+        cin >> n;
+        cout << ceil(n / 4) << endl;
+    }
+
+    return 0;
+}
