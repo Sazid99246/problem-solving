766ccf67ca668d6801a070498417d377d4c54024
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-07-30 16:50:30 +0600

    FSQRT problem solved

---
+++ Codechef/FSQRT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        double n;
+        cin >> n;
+        cout << round(sqrt(n)) << endl;
+    }
+
+    return 0;
+}
