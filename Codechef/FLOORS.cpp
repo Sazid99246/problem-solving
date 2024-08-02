0d5d12074471e12c0cf4f70fac093671297572d2
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-08-02 18:02:17 +0600

    FLOORS problem solved

---
+++ Codechef/FLOORS.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find_floor(int x)
+{
+    if (x % 10 == 0)
+        return x / 10;
+    else
+        return x / 10 + 1;    
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int chef_floor = find_floor(x);
+        int chefina_floor = find_floor(y);
+
+        if (chefina_floor > chef_floor)
+        {
+            swap(chef_floor, chefina_floor);
+        }
+        cout << chef_floor - chefina_floor << endl;
+    }
+    
+    return 0;
+}
