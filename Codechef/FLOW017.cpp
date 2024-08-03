dd0676543b91693c3d2c68d45e903dced9c06d83
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-08-03 08:48:41 +0600

    FLOW017 problem solved

---
+++ Codechef/FLOW017.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int arr[3] = {a, b, c};
+        sort(arr, arr + 3);
+        cout << arr[1] << endl;
+    }
+
+    return 0;
+}
