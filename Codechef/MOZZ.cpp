697215b39075d75c04abaee1b56dcbdc1f3a8c54
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-08-04 06:56:40 +0600

    MOZZ problem solved

---
+++ Codechef/MOZZ.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        double x, y, r;
+        cin >> x >> y >> r;
+        int extra_sticks = r / 30;
+        int total_sticks = extra_sticks + x;
+        cout << ceil(total_sticks / y) << endl;
+    }
+    
+    return 0;
+}
