21f40da4b0a2ad1dc96be34bed7ecc9efb525640
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-08-04 07:13:12 +0600

    FLOW018 problem solved

---
+++ Codechef/FLOW018.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << factorial(n) << endl;
+    }
+    
+    return 0;
+}
