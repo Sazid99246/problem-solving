aef9e9954646182d18fcb89fc9cf803f5adf20b4
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-08-06 06:49:00 +0600

    MONEYDOUBLE problem solved

---
+++ Codechef/MONEYDOUBLE.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int total = x;
+        for (int i = 1; i <= y; i++)
+        {
+            total = max(total + 1000, total * 2);
+        }
+        cout << total << endl;
+    }
+    
+    return 0;
+}
