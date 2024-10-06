c6c6876b4aac979f09123c17aa684b19dc26b02d
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-06 23:22:32 +0600

    carrots problem solved

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    for (int i = 0; i < n; i++)
+    {
+        string status;
+        cin >> status;
+    }
+    cout << p << endl;
+    return 0;
+}
