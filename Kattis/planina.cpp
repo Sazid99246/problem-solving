88df77f11822dfaa77f19fb3b99a2e6861a0bc65
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-08 06:39:13 +0600

    planina problem solved

---
+++ Kattis/planina.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    long long points = pow((pow(2, n) + 1), 2);
+    cout << points << endl;
+    return 0;
+}
