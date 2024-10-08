542a8daf0636f0bf841d750811c4feb1a2c3a043
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-08 09:37:45 +0600

    difference problem solved

---
+++ Kattis/different.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long x, y;
+    while (cin >> x >> y)
+    {
+        cout << abs(x - y) << endl;
+    }
+    
+    return 0;
+}
