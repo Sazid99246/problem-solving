452230475b17ad7b6f5e2c0fa59120048060fea3
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-09 20:16:41 +0600

    12372 problem solved

---
+++ uVa/12372.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        int l, w, h;
+        cin >> l >> w >> h;
+        cout << "Case " << i << ": ";
+        if (l <= 20 && w <= 20 && h <= 20)
+        {
+            cout << "good" << endl;
+        }
+        else
+        {
+            cout << "bad" << endl;
+        }
+    }
+
+    return 0;
+}
