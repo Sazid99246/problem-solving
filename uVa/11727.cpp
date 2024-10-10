68be3eef1931b462d213a0cb4d025fd58044dce8
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-10 11:08:23 +0600

    11727 problem solved

---
+++ uVa/11727.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        vector<int> nums;
+        int a, b, c, mid = 0;
+        cin >> a >> b >> c;
+        nums.push_back(a);        
+        nums.push_back(b);        
+        nums.push_back(c);        
+        sort(nums.begin(), nums.end());
+        cout << "Case " << i << ": " << nums[1] << endl;
+    }
+    
+    return 0;
+}
