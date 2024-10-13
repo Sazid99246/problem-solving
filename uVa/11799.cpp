74f4373e0ddd1beebf8cb858870c5a3f24860eb1
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-10-13 07:40:12 +0600

    UVA 11799 problem solved

---
+++ uVa/11799.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        cin >> n;
+        vector<int> nums;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            nums.push_back(x);
+        }
+
+        cout << "Case " << i << ": " << *max_element(nums.begin(), nums.end()) << endl;
+    }
+
+    return 0;
+}
